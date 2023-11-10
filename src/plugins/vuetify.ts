372c7688432cdb14ef1589d0dcba2bb25b8e3503
KevinT3Hu <kevint3hu@outlook.com>
2023-11-10 13:53:46 +0800

    perf: optimize style settings
    Mannually set --v-theme-overlay-multiplier since this seems missing

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,5 +4,22 @@ import { createVuetify } from 'vuetify'
 export default createVuetify({
     icons:{
         defaultSet: 'mdi'
+    },
+    theme:{
+        defaultTheme: 'light',
+        themes:{
+            light:{
+                dark: false,
+                colors:{
+                    primary: '#FF5722',
+                    secondary: '#FF9800',
+                    accent: '#FFC107',
+                    error: '#F44336',
+                    warning: '#FFEB3B',
+                    info: '#03A9F4',
+                    success: '#4CAF50',
+                }
+            }
+        }
     }
 })
